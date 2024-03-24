c08996447a5efb816ff048052cf8ef24dc185a18
biffthundermuffin <150493877+biffthundermuffin@users.noreply.github.com>
2024-03-24 14:44:44 -0700

    Update scheduled-runner.py
    updated to print when successfully ran

--- scheduled-runner.py
+++ scheduled-runner.py
@@ -4,7 +4,8 @@ import subprocess
 
 def run_script():
     try:
-        subprocess.run(["python", "python to call here.py"], check=True)
+        subprocess.run(["python", "scripttorun.py"], check=True)
+        print("Script called successfully")
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
 
